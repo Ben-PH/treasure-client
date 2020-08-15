e09966d4a1568cd9e38f8cefa3a94c13aa94554e
Ben-PH <benphawke@gmail.com>
2020-08-15 08:30:35 +0200

    Implement Login

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,77 @@
 use seed::{prelude::*, *};
 
+mod pages;
 
-struct Model;
+fn init(mut _url: Url, orders: &mut impl Orders<Message>) -> Model {
+    log!("I N I T I A L I Z E");
 
-fn init(url: Url, orders: &mut impl Orders<Message>) -> Model {
-    Model
+    orders.perform_cmd(async {
+        match Request::new("/api/auth").method(Method::Get).fetch().await {
+            Ok(fetch) => match fetch.check_status() {
+                Ok(good_resp) => Message::LoginMsg(pages::login::Message::GoodLogin(
+                    good_resp.json().await.unwrap(),
+                )),
+                Err(_) => Message::LoginMsg(pages::login::Message::Unauth),
+            },
+            Err(e) => Message::NetworkError(e),
+        }
+    });
+    Model::default()
+}
+impl Model {
+    fn default() -> Self {
+        Self {
+            login: Some(pages::login::Model::default())
+        }
+    }
 }
 
-enum Message {
-    Nothing
+#[derive(Default, Debug)]
+struct Model {
+    login: Option<pages::login::Model>,
 }
 
-fn update(msg: Message, model: &mut Model, orders: &mut impl Orders<Message>) {}
+#[derive(Debug)]
+pub enum Message {
+    GoodLogin(shared::User),
+    LoginMsg(pages::login::Message),
+    NetworkError(fetch::FetchError),
+}
 
-fn view(mdl: &Model) -> impl IntoNodes<Message> {
-    empty![]
+// ------ ------
+//    Update
+// ------ ------
+
+fn update(msg: Message, model: &mut Model, orders: &mut impl Orders<Message>) {
+    log("updating");
+    use Message::*;
+    match msg {
+        GoodLogin(usr) => {
+            model.login = None;
+            log!("good login");
+        }
+        LoginMsg(msg) => {
+            if let Some(GoodLogin(usr)) = pages::login::update(
+                msg,
+                model.login.as_mut().unwrap(),
+                &mut orders.proxy(Message::LoginMsg),
+            ) {
+                orders.perform_cmd(async move { GoodLogin(usr) });
+            }
+        },
+        _ => log!("impl me: ", msg),
+    }
+}
+
+// ------ ------
+//     View
+// ------ ------
+
+fn view(mdl: &Model) -> Vec<Node<Message>> {
+    match &mdl.login {
+        Some(login) => nodes![pages::login::view(&login)].map_msg(Message::LoginMsg),
+        None => nodes![div![format!("logged-in:\n{:?}", mdl)]]
+    }
 }
 
 #[wasm_bindgen(start)]
---
+++ src/pages/login.rs
@@ -0,0 +1,148 @@
+
+use seed::{prelude::*, *};
+use serde::{Deserialize, Serialize};
+
+use shared;
+// ------ ------
+//     Init
+// ------ ------
+
+pub fn init() -> Model {
+    Model::default()
+}
+
+// ------ ------
+//     Model
+// ------ ------
+
+#[derive(Debug, Default)]
+pub struct Model {
+    sent: bool,
+    good_log: bool,
+    form: shared::Login,
+}
+// ------ ------
+//     Update
+// ------ ------
+
+#[derive(Debug)]
+pub enum Message {
+    Unauth,
+    LoggedOut,
+    ChangeEmail(String),
+    ChangePassword(String),
+    LoginSent(fetch::Response),
+    GoodLogin(shared::User),
+    ParsedResp(shared::User),
+    BadLogin(fetch::FetchError),
+    LoginClicked,
+    NetworkError(fetch::FetchError),
+}
+
+pub fn update(
+    msg: Message,
+    model: &mut Model,
+    orders: &mut impl Orders<Message>,
+) -> Option<crate::Message> {
+    log!("login page update");
+    use Message::*;
+    match msg {
+        Unauth => {
+            log!("unauth");
+            *model = Model::default();
+        }
+        ChangeEmail(new) => model.form.email = new,
+        ChangePassword(new) => {
+            model.form.password = new;
+        }
+        LoginClicked => {
+            orders.skip();
+            let resp = Request::new("api/auth/login")
+                .method(Method::Post)
+                .json(&model.form)
+                .expect("bad serialization")
+                .fetch();
+            model.form.password = "".to_string();
+            orders.perform_cmd(async {
+                match resp.await {
+                    Ok(fired) => LoginSent(fired),
+                    Err(e) => NetworkError(e),
+                }
+            });
+        }
+        LoginSent(resp) => {
+            // set the submitted state login is sent
+            if model.sent {
+                orders.skip();
+            }
+            model.sent = true;
+            match resp.check_status() {
+                Ok(good_resp) => {
+                    orders.perform_cmd(async move {
+                        GoodLogin(good_resp.json::<shared::User>().await.unwrap())
+                    });
+                }
+                Err(e) => {
+                    orders.perform_cmd(async { BadLogin(e) });
+                }
+            }
+        }
+        BadLogin(e) => {
+            log!(e);
+            model.good_log = false;
+        }
+        GoodLogin(usr) => return Some(crate::Message::GoodLogin(usr)),
+        _ => log!("impl me: ", msg),
+    }
+    None
+}
+
+pub fn view(creds: &Model) -> impl IntoNodes<Message> {
+    form![
+        ev(Ev::Submit, |event| {
+            event.prevent_default();
+            Message::LoginClicked
+        }),
+        fieldset![
+            // attrs! {
+            //     At::Disabled=> status.as_at_value(),
+            // },
+            legend!["credentials"],
+            ul![
+                li![
+            label![attrs! { At::For => "username"}],
+            input![
+                attrs! {
+                    At::Required => true,
+                    At::Value=> creds.form.email,
+                    At::Name => "username",
+                    At::Type=> "email",
+                    At::Placeholder => "Email"
+
+                },
+                input_ev(Ev::Input, Message::ChangeEmail),
+            ]
+                ],
+                li![
+            label![attrs! { At::For => "password"} ],
+            input![
+                attrs! {
+                    At::Required => true,
+                    At::Value => creds.form.password,
+                    At::Type=> "password",
+                    At::Placeholder => "Password"
+                },
+                input_ev(Ev::Input, Message::ChangePassword),
+            ]
+                ]
+            ]
+
+        ],
+        button![
+            "Login",
+            attrs! {
+                At::Type=> "submit"
+            },
+        ]
+    ]
+}
